36deeac69a5c08c084de15343689ab11760dc2a1
maiurysousa <maiuryprogramando@gmail.com>
2021-11-27 11:16:28 -0300

    adicionando stilo

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://avatars.githubusercontent.com/u/82973647?v=4" alt="Minha  foto de perfil" className="img" />
+        </>
+    );
+}
+export default Home;
