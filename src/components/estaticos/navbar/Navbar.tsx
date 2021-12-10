c9a631054f656b5e079227d47ecd792276eaf4e6
maiurysousa <maiuryprogramando@gmail.com>
2021-12-10 16:27:40 -0300

    adicionando mensagens de alert personalizadas

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -5,83 +5,109 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import useLocalStorage from 'react-use-localstorage';
 import { Link, useHistory } from 'react-router-dom';
 import './Navbar.css';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { useDispatch } from "react-redux";
+import { addToken } from '../../../store/tokens/actions';
+import {toast} from 'react-toastify';
 
 function Navbar() {
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+      );
 
-    let history = useHistory(); // para redirecionar
-    const [token, setToken] = useLocalStorage('token'); // para guardar o token no localstorage
-    function logout() {
-        setToken(''); // para apagar o token do localstorage
+    let history = useHistory();
+    const dispatch = useDispatch(); //dispara a ação
+  
+    function goLogout() {
+        dispatch(addToken(''));
+        toast.info('Usuário deslogado', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "colored",
+            progress: undefined,
+        });
         history.push('/login'); // para redireccionar a página de login
     }
 
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar variant="dense" className="color-navbar">
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        style={{ color: "#FFE9C9" }}
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
+    var navbarComponent;
 
-                    <Box paddingX={3.5} className="cursor" >
-                        <Link to='/home' className='text-decoration-none'>
-                            <Typography variant="h5" component="div" className="fonte" sx={{ flexGrow: 1 }}>
-                                FlóreoBlog
-                            </Typography>
-                        </Link>
-                    </Box>
+    if(token != ""){
+        navbarComponent = <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+            <Toolbar variant="dense" className="color-navbar">
+                <IconButton
+                    size="large"
+                    edge="start"
+                    style={{ color: "#FFE9C9" }}
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                >
+                    <MenuIcon />
+                </IconButton>
 
+                <Box paddingX={3.5} className="cursor" >
                     <Link to='/home' className='text-decoration-none'>
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" component="div" className="fonte" sx={{ flexGrow: 1 }}>
-                                home
-                            </Typography>
-                        </Box>
+                        <Typography variant="h5" component="div" className="fonte" sx={{ flexGrow: 1 }}>
+                            FlóreoBlog
+                        </Typography>
                     </Link>
+                </Box>
 
-                    <Link to="/posts" className="text-decorator-none">
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" component="div" className="fonte" sx={{ flexGrow: 1 }}>
-                                postagens
-                            </Typography>
-                        </Box>
-                    </Link>
-
-                    <Link to="/tema" className="text-decorator-none">
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" component="div" className="fonte" sx={{ flexGrow: 1 }}>
-                                temas
-                            </Typography>
-                        </Box>
-                    </Link>
-
-                    <Link to="/formularioTema" className="text-decorator-none">
-                        <Box mx={1} className="cursor">
-                            <Typography variant="h6" component="div" className="fonte" sx={{ flexGrow: 1 }}>
-                                cadastrar tema
-                            </Typography>
-                        </Box>
-                    </Link>
-
-                    <Box paddingLeft={70} mx={1} className="cursor">
-                        <Typography variant="h6" component="div" className="fonte" sx={{ flexGrow: 1 }} onClick={() => logout()}>
-                            logout
+                <Link to='/home' className='text-decoration-none'>
+                    <Box mx={1} className="cursor">
+                        <Typography variant="h6" component="div" className="fonte" sx={{ flexGrow: 1 }}>
+                            home
                         </Typography>
                     </Box>
+                </Link>
+
+                <Link to="/posts" className="text-decorator-none">
+                    <Box mx={1} className="cursor">
+                        <Typography variant="h6" component="div" className="fonte" sx={{ flexGrow: 1 }}>
+                            postagens
+                        </Typography>
+                    </Box>
+                </Link>
+
+                <Link to="/tema" className="text-decorator-none">
+                    <Box mx={1} className="cursor">
+                        <Typography variant="h6" component="div" className="fonte" sx={{ flexGrow: 1 }}>
+                            temas
+                        </Typography>
+                    </Box>
+                </Link>
+
+                <Link to="/formularioTema" className="text-decorator-none">
+                    <Box mx={1} className="cursor">
+                        <Typography variant="h6" component="div" className="fonte" sx={{ flexGrow: 1 }}>
+                            cadastrar tema
+                        </Typography>
+                    </Box>
+                </Link>
+
+                <Box paddingLeft={70} mx={1} className="cursor" onClick={() => goLogout()}>
+                    <Typography variant="h6" component="div" className="fonte" sx={{ flexGrow: 1 }} >
+                        logout
+                    </Typography>
+                </Box>
 
 
-                </Toolbar>
-            </AppBar>
-        </Box>
+            </Toolbar>
+        </AppBar>
+    </Box>
+    }
+
+    return (
+        <>
+        {navbarComponent}
+        </> 
     )
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -6,7 +6,7 @@ import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostage
 import './Home.css';
 import { useSelector } from 'react-redux';
 import { TokenState } from '../../store/tokens/tokensReducer';
-
+import {toast} from 'react-toastify';
 
 function Home() {
 
@@ -17,7 +17,16 @@ function Home() {
 
     useEffect(() => {
         if(token == ''){
-            alert("Você precisa estar logado!")
+            toast.error('Você precisa estar logado', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "colored",
+                progress: undefined,
+            });
             history.push('/login')
         }
 
@@ -37,7 +46,9 @@ function Home() {
                         <Box marginRight={1}>
                         <ModalPostagem />
                         </Box>
+                        <Link to="/postagens">
                         <Button variant="outlined" className="botao">Ver Postagens</Button>
+                        </Link>
                     </Box>
                 </Grid>
 
